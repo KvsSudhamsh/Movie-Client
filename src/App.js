bd957e2745d5fb010b16133b4dfa6d496273e8d6
vishnukvs <vishnu.kvs202@gmail.com>
2023-07-29 00:06:57 -0700

    created and implemted layout.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import api from "./api/axiosConfig";
 import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   const [movies, setMovies] = useState();
@@ -27,6 +29,13 @@ useEffect(()=>{
   return (
     <div className="App">
       
+      <Routes>
+        <Route path='/' element={Layout}>
+          
+
+        </Route>
+      </Routes>
+
     </div>
   );
 }
